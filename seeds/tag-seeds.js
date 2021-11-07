43b88d228d3efa1c09b31a15891ad4e4ea4558d9
Ayla122 <ayladd122@gmail.com>
2021-11-06 21:38:34 -0700

    structured the zmodels files

--- seeds/tag-seeds.js
+++ seeds/tag-seeds.js
@@ -1,1 +1,35 @@
 const { Tag } = require('../models');
+
+const tagData = [
+  {
+    tag_name: '',
+  },
+  {
+    tag_name: '',
+  },
+  {
+    tag_name: '',
+  },
+  {
+    tag_name: '',
+  },
+  {
+    tag_name: '',
+  },
+  {
+    tag_name: '',
+  },
+  {
+    tag_name: '',
+  },
+  {
+    tag_name: '',
+  },
+  {
+    tag_name: '',
+  },
+];
+
+const seedTags = () => Tag.bulkCreate(tagData);
+
+module.exports = seedTags;
